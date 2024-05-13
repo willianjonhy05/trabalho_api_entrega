6975195c8622abe78efe082fd124c2ff2a98e68b
willianjonhy05 <willianjohny05@gmail.com>
2024-05-13 20:04:47 -0300

    Criação do admin

--- escola/models.py
+++ escola/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=30)
@@ -8,23 +7,19 @@ class Aluno(models.Model):
     rg = models.CharField(max_length=20)
     data_nascimento = models.CharField(max_length=10)
     
-    
     def __str__(self):
         return self.nome
-
 
     class Meta:
         verbose_name = 'Aluno'
         verbose_name_plural = 'Alunos'
     
-
 class Idioma(models.Model):
     codigo = models.CharField(max_length=11)
     nome = models.CharField(max_length=30)
     
     def __str__(self):
         return self.nome
-    
 
     class Meta:
         verbose_name = 'Idioma'
@@ -41,8 +36,6 @@ class Professor(models.Model):
     def __str__(self):
         return self.nome
     
-
     class Meta:
         verbose_name = 'Professor'
         verbose_name_plural = 'Professores'
-    
