ff3963f7040ec697914e130b45fb62d8dc053aa4
willianjonhy05 <willianjohny05@gmail.com>
2024-05-13 20:59:40 -0300

    Configuração de URLs

--- escola/urls.py
+++ escola/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index
+from .views import index, AlunoListCreate, getRoutes
 
 urlpatterns = [
-    path('', index),
+    path('alunos/', AlunoListCreate.as_view(), name='alunos-create-list'),
+    path('', getRoutes, name='get-routes'),    
     
 ]
--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Aluno
+from .serializer import AlunoSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'home.html')
+
+@api_view(["GET"])
+def getRoutes(request):
+    routes = {
+    "Alunos": "http://127.0.0.1:8000/alunos/",
+
+}
+    return Response(routes)
+
+
+class AlunoListCreate(generics.ListCreateAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
