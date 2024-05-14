c51250c7c69a8bb9f52781107048b451ddc418ca
willianjonhy05 <willianjohny05@gmail.com>
2024-05-13 21:21:37 -0300

    Novas funcionalidades

--- escola/urls.py
+++ escola/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-from .views import index, AlunoListCreate, getRoutes
+from .views import *
 
 urlpatterns = [
+    path('professor/detail/<int:pk>/', ProfessorRetrieveUpdateDestroy.as_view(), name='professor-detail'),
+    path('professor/', ProfessorListCreate.as_view(), name='professor-create-list'),
+    path('idioma/detail/<int:pk>/', IdiomaRetrieveUpdateDestroy.as_view(), name='idioma-detail'),
+    path('idioma/', IdiomaListCreate.as_view(), name='idioma-create-list'),
+    path('alunos/detail/<int:pk>/', AlunoRetrieveUpdateDestroy.as_view(), name='aluno-detail'),
     path('alunos/', AlunoListCreate.as_view(), name='alunos-create-list'),
     path('', getRoutes, name='get-routes'),    
     
--- escola/views.py
+++ escola/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Aluno
-from .serializer import AlunoSerializer
+from .models import Aluno, Professor, Idioma
+from .serializer import AlunoSerializer, ProfessorSerializer, IdiomaSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'home.html')
-
 @api_view(["GET"])
 def getRoutes(request):
     routes = {
     "Alunos": "http://127.0.0.1:8000/alunos/",
+    "Idiomas": "http://127.0.0.1:8000/idioma/",
+    "Professor": "http://127.0.0.1:8000/professor/",
 
 }
     return Response(routes)
 
-
 class AlunoListCreate(generics.ListCreateAPIView):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+
+class AlunoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class ProfessorListCreate(generics.ListCreateAPIView):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+class ProfessorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+class IdiomaListCreate(generics.ListCreateAPIView):
+    queryset = Idioma.objects.all()
+    serializer_class = IdiomaSerializer
+
+class IdiomaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Idioma.objects.all()
+    serializer_class = IdiomaSerializer
